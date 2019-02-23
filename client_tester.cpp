dcae847f4905302a3b4b84c7c77fe364c01c7b16
x2009 <george.glavan27@gmail.com>
2019-02-23 13:01:52 +0200

    add comments

--- client_tester.cpp
+++ client_tester.cpp
@@ -1,7 +1,7 @@
 /*
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         To compile in windows using g++, u 
-        need to add this flags to gcc cmd:
+        need to add this flags to g++ cmd:
 
                     -lws2_32
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -11,13 +11,18 @@
 
 int main()
 {
-    sock::init();
+    sock::init(); // init WSADATA in windows
 
-    sock::UniversalSocket s;
+    sock::UniversalSocket s; // declare our socket so we have a way to connect.
+                             // This is equivalent with declaring a file descriptor
+                             // int fd = 0;
 
-    int res = sock::connect(s, "localhost", 5555);
-
-    if (res)
+    int res = sock::connect(s, "localhost", 5555); // connect to `localhost` on port 55555
+                                                   // this is equivalent with an open(fd)
+                                                   // localhost is our computer. We coud
+                                                   // have connected to `google.ro` for example
+                                                   // or to `8.8.8.8`
+    if (res)                                       // check for errors
     {
         fprintf(stderr, "connect failed with %d\n", res);
         sock::uninit(s);
@@ -28,11 +33,12 @@ int main()
         printf("connect succes!\n");
     }
 
-    std::string payload = "asd";
+    std::string payload = "asd"; // prepare our message
 
-    res = sock::send(s, payload);
+    res = sock::send(s, payload); // send our message to the server we connected to
+                                  // this is equivalent to a write
 
-    if (res < 0)
+    if (res < 0) // check for errors
     {
         fprintf(stderr, "send failed with %d\n", res);
         sock::uninit(s);
@@ -45,10 +51,18 @@ int main()
 
     char recv_buf[100];
 
-    res = sock::recv(s, recv_buf, 99);
+    res = sock::recv(s, recv_buf, 99); // receive our message from the server
+                                       // this is equivalent to a read
+                                       // not that this call to recv will return
+                                       // the number of bytes received from server
+                                       // this can be < 99.
+                                       // Also very important: this call to recv is
+                                       // blocking until we receive somentring from
+                                       // the network. So in case the server doesnt
+                                       // send anything back, we are blocked here
     recv_buf[100] = '\0';
 
-    if (res < 0)
+    if (res < 0) // check for errors
     {
         fprintf(stderr, "recv failed with %d\n", res);
         sock::uninit(s);
@@ -59,7 +73,8 @@ int main()
         printf("Got response from server: %s\n", recv_buf);
     }
 
-    sock::disconnect(s);
+    sock::disconnect(s); // disconnect
+                         // this is equivalent to a close
 
-    sock::uninit(s);
+    sock::uninit(s); // uninit WSADATA
 }
--- server_tester.cpp
+++ server_tester.cpp
@@ -1,7 +1,7 @@
 /*
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         To compile in windows using g++, u 
-        need to add this flags to gcc cmd:
+        need to add this flags to g++ cmd:
 
                     -lws2_32
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -11,13 +11,15 @@
 
 int main()
 {
-    sock::init();
+    sock::init(); // init WSADATA
 
-    sock::UniversalSocket s;
+    sock::UniversalSocket s; // declare our socket so we have a way to listen for connections.
+                             // This is equivalent with declaring a file descriptor
+                             // int fd = 0;
 
-    int res = sock::listen(s, 5555);
+    int res = sock::listen(s, 5555); // start listening for new connections on port 5555
 
-    if (res)
+    if (res) // check for errors
     {
         fprintf(stderr, "listen failed with %d\n", res);
         sock::uninit(s);
@@ -28,7 +30,18 @@ int main()
         printf("listen succes!\n");
     }
 
-    sock::UniversalSocket client = sock::accept(s);
+    sock::UniversalSocket client = sock::accept(s); // accept a new connection
+                                                    // this call to accept will return a new
+                                                    // UniversalSocket that we can use as a connected socket
+                                                    // note that s (our listening socket) is used only to listen
+                                                    // for new connection. Once we have one, we can read and write
+                                                    // to our new created socket.
+                                                    // Also very important: this call to accept is blocking until
+                                                    // a client is `connect`-ing to our server. We can wait here
+                                                    // forever
+
+    // from here on, everything is the same as in client_tester. Only the order of read and write differs.
+    // If you want to check the comments, go to client_tester.cpp because i'm too lazy to copy them here :D
 
     if (client == sock::INV_SOCK)
     {
